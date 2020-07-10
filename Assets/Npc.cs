4fafcb312d85748e74183548909443d364b0d26d
Swofty <swiftstick@gmail.com>
2020-07-10 00:05:20 -0700

    Added stage based functions for NPC as an example

--- Assets/Npc.cs
+++ Assets/Npc.cs
@@ -128,6 +128,77 @@ public class Npc : MonoBehaviour
         Shard s = new Shard("0", 0, "testing");
         ReisenGameManager.instance.gameProgress.Player.AddShard(s);
     }
+
+    public ReisenGameProgress GameProgress => ReisenGameManager.instance.gameProgress;
+    public void Kosuzu_Stage000_CoughMedicine()
+    {
+        GameProgress.Player.CoughingMedicine = Assignment.Kosuzu;
+        GameProgress.Player.TextBook = Assignment.Inventory;
+        GameProgress.Player.AddShard(Shard.Kosuzu_CoughingMedicine);
+        GameProgress.Kosuzu.Stage = 100;
+    }
+
+    public void Kosuzu_Stage000_Elixir()
+    {
+        AssignAvailableElixir(Assignment.Kosuzu);
+        GameProgress.Kosuzu.Stage = 1100;
+    }
+
+    public void Kosuzu_Stage100_Correct()
+    {
+        GameProgress.Player.AddShard(Shard.Kosuzu_QuestionCorrect);
+        GameProgress.Kosuzu.Stage = 200;
+    }
+
+    public void Kosuzu_Stage100_Wrong()
+    {
+        GameProgress.Kosuzu.Stage = 200;
+    }
+
+    public void Kosuzu_Stage200_Elixir()
+    {
+        GameProgress.Kosuzu.Stage = 1100;
+    }
+
+    public void Kosuzu_Stage200_Magazine()
+    {
+        GameProgress.Player.Magazine = Assignment.Kosuzu;
+        GameProgress.Player.AddShard(Shard.Kosuzu_MagazineBadEnd);
+        GameProgress.Kosuzu.Stage = 600;
+    }
+
+    public void Kosuzu_Stage200_Scroll()
+    {
+        GameProgress.Player.Scroll = Assignment.Kosuzu;
+        GameProgress.Kosuzu.Stage = 1000;
+    }
+
+    public void Kosuzu_Stage1000()
+    {
+        GameProgress.Player.AddShard(Shard.Kosuzu_GoodEnd);
+        GameProgress.Kosuzu.Stage = 1001;
+    }
+
+    public void Kosuzu_Stage1001()
+    {
+        GameProgress.Player.Schematic = Assignment.Inventory;
+    }
+
+    public void AssignAvailableElixir(Assignment a)
+    {
+        if (GameProgress.Player.Elixir1 == Assignment.Inventory)
+        {
+            GameProgress.Player.Elixir1 = a;
+        }
+        else if (GameProgress.Player.Elixir2 == Assignment.Inventory)
+        {
+            GameProgress.Player.Elixir2 = a;
+        }
+        else
+        {
+            throw new InvalidOperationException("Tried to assign Elixir, but player does not have any in inventory");
+        }
+    }
 }
 
 [Serializable]
