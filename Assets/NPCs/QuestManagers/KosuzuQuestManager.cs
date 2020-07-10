1d97fb630cc29d930a22454fb9ee77f16af0ac53
Swofty <swiftstick@gmail.com>
2020-07-10 01:13:31 -0700

    Suzunnan rigged up

--- Assets/NPCs/QuestManagers/KosuzuQuestManager.cs
+++ Assets/NPCs/QuestManagers/KosuzuQuestManager.cs
@@ -1,19 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class KosuzuQuestManager : MonoBehaviour
+﻿public class KosuzuQuestManager : Npc
 {
-    // Start is called before the first frame update
-    public void addFirstShard()
+    public void Kosuzu_Stage000_CoughMedicine()
     {
-        Debug.Log("adding shard");
+        GameProgress.Player.CoughingMedicine = Assignment.Kosuzu;
+        GameProgress.Player.TextBook = Assignment.Inventory;
+        GameProgress.Player.AddShard(Shard.Kosuzu_CoughingMedicine);
+        GameProgress.Kosuzu.Stage = 100;
     }
 
-    // Start is called before the first frame update
-    public void addBook()
+    public void Kosuzu_Stage000_Elixir()
     {
-        ReisenGameManager.instance.gameProgress.Player.TextBook = Assignment.Inventory;
-        Debug.Log("adding book");
+        AssignAvailableElixir(Assignment.Kosuzu);
+        GameProgress.Kosuzu.Stage = 1100;
+    }
+
+    public void Kosuzu_Stage100_Correct()
+    {
+        GameProgress.Player.AddShard(Shard.Kosuzu_QuestionCorrect);
+        GameProgress.Kosuzu.Stage = 200;
+    }
+
+    public void Kosuzu_Stage100_Wrong()
+    {
+        GameProgress.Kosuzu.Stage = 200;
+    }
+
+    public void Kosuzu_Stage200_Elixir()
+    {
+        GameProgress.Kosuzu.Stage = 1100;
+    }
+
+    public void Kosuzu_Stage200_Magazine()
+    {
+        GameProgress.Player.Magazine = Assignment.Kosuzu;
+        GameProgress.Player.AddShard(Shard.Kosuzu_MagazineBadEnd);
+        GameProgress.Kosuzu.Stage = 600;
+    }
+
+    public void Kosuzu_Stage200_Scroll()
+    {
+        GameProgress.Player.Scroll = Assignment.Kosuzu;
+        GameProgress.Kosuzu.Stage = 1000;
+    }
+
+    public void Kosuzu_Stage1000()
+    {
+        GameProgress.Player.AddShard(Shard.Kosuzu_GoodEnd);
+        GameProgress.Kosuzu.Stage = 1001;
+    }
+
+    public void Kosuzu_Stage1001()
+    {
+        GameProgress.Player.Schematic = Assignment.Inventory;
+    }
+
+    public void Kosuzu_Stage1100()
+    {
+        GameProgress.Player.AddShard(Shard.Kosuzu_Elixir);
+        GameProgress.Kosuzu.Stage = 1101;
     }
 }
