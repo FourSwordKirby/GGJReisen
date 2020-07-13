36f2f9b55735aa9d269e19ce0bcb9695c44aea5c
Swofty <swiftstick@gmail.com>
2020-07-13 04:57:25 -0700

    Fix wall generators. Align Nitori's scene to y=0 for floor

--- Assets/Tools/WallGenerator.cs
+++ Assets/Tools/WallGenerator.cs
@@ -7,8 +7,9 @@ public class WallGenerator : MonoBehaviour
 
     //PUBLIC INSPECTOR VARIABLES
     [SerializeField]
-    public GameObject go_next_fence_post;
-    public Vector3 GizmoScale;
+    public WallGenerator go_next_fence_post;
+    public Vector3 FencePostSize = new Vector3(.2f, 2f, .2f);
+    public Vector3 FencePostCenter = new Vector3(0f, .5f, .2f);
 
     //PRIVATE VARIABLES
 
@@ -25,20 +26,16 @@ public class WallGenerator : MonoBehaviour
 
     void Awake()
     {
-        transform.localScale = GizmoScale;
-        //hide this fence post
-        Destroy(GetComponent<MeshRenderer>());
-
         //is there another fence post?
         if (go_next_fence_post != null)
         {
 
             //get Vector3's that are half the length of the fence pole heights, with the same rotations
-            v3_thishalflength = transform.TransformDirection(Vector3.up * transform.lossyScale.y / 2);
-            v3_nexthalflength = go_next_fence_post.transform.TransformDirection(Vector3.up * go_next_fence_post.transform.lossyScale.y / 2);
+            v3_thishalflength = transform.TransformVector(Vector3.up * FencePostSize.y / 2);
+            v3_nexthalflength = go_next_fence_post.transform.TransformVector(Vector3.up * go_next_fence_post.FencePostSize.y / 2);
 
-            //get the Vector3 that's the distance  direction between these two fence posts
-            v3_distance = go_next_fence_post.transform.position - transform.position;
+            //get the Vector3 that's the distance direction between these two fence posts
+            v3_distance = go_next_fence_post.FencePostCenterWorldSpace - FencePostCenterWorldSpace;
         }
     }
 
@@ -51,6 +48,9 @@ public class WallGenerator : MonoBehaviour
             transform.localScale = new Vector3(1, 1, 1);
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
 
+            v3_nexthalflength = transform.InverseTransformVector(v3_nexthalflength);
+            v3_distance = transform.InverseTransformVector(v3_distance);
+
             //create the mesh filter
             if (GetComponent<MeshFilter>() == null)
             {
@@ -60,7 +60,11 @@ public class WallGenerator : MonoBehaviour
 
             //create the 4 vertices
             ms_mesh.Clear();
-            ms_mesh.vertices = new Vector3[] { v3_thishalflength, -v3_thishalflength, v3_distance + v3_nexthalflength, v3_distance - v3_nexthalflength };
+            ms_mesh.vertices = new Vector3[] {
+                FencePostCenter + v3_thishalflength,
+                FencePostCenter - v3_thishalflength,
+                FencePostCenter + v3_distance + v3_nexthalflength,
+                FencePostCenter + v3_distance - v3_nexthalflength };
 
             //create 4 triangles between the 2 fence posts, to cover the space between them thoroughly.  Create each triangle in 2 ways (like 0,1,2 and 0,2,1) so they're impenetrable from both sides
             ms_mesh.triangles = new int[] { 0, 1, 2, 0, 2, 1, 0, 1, 3, 1, 3, 1, 0, 2, 3, 0, 3, 2, 1, 2, 3, 1, 3, 2 };
@@ -82,13 +86,15 @@ public class WallGenerator : MonoBehaviour
         }
     }
 
+    public Vector3 FencePostCenterWorldSpace => transform.TransformPoint(FencePostCenter);
+
     public void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawCube(this.transform.position, GizmoScale);// + Vector3.up * (this.transform.localScale.y - 1));
+        Gizmos.DrawCube(FencePostCenterWorldSpace, FencePostSize);
 
         Gizmos.color = Color.yellow;
         if(go_next_fence_post != null)
-            Gizmos.DrawLine(this.transform.position, this.go_next_fence_post.transform.position);
+            Gizmos.DrawLine(FencePostCenterWorldSpace, this.go_next_fence_post.FencePostCenterWorldSpace);
     }
 }
