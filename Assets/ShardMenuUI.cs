66d78a04d7850e66a2df2204467217ffe884a8cb
RogPodge <roliu@microsoft.com>
2020-07-13 00:50:21 -0700

    mostly functional pause UI, need to test on real game state

--- Assets/ShardMenuUI.cs
+++ Assets/ShardMenuUI.cs
@@ -6,6 +6,8 @@ public class ShardMenuUI : MenuUI
 {
     public bool isPauseMenuVersion;
 
+    public PauseMenuUI pauseMenuUI;
+
     public GameObject shardMenuElement;
     public Transform initialPosition;
     public int rows;
@@ -13,34 +15,54 @@ public class ShardMenuUI : MenuUI
 
     public float xspacing;
     public float yspacing;
+    
 
     public override void Init()
     {
         ReisenGameProgress gameProgress = ReisenGameManager.instance.gameProgress;
 
-
         List<Shard> shardsToDisplay;
 
         if (isPauseMenuVersion)
-            shardsToDisplay = gameProgress.Player.ShardsAcquired;
+            shardsToDisplay = new List<Shard>();
         else
             shardsToDisplay = new List<Shard>() { new Shard("0", 0, "testing", "why not"), new Shard("0", 0, "testing2", "why not2") };
 
-        if (shardsToDisplay.Count == 0)
-            Debug.Log("Insert edge case of reisen playing making a quip");
+        foreach (MenuUIElement oldElement in group.menuElements)
+        {
+            Destroy(oldElement.gameObject);
+        }
+        group.menuElements.Clear();
 
         for (int i = 0; i < shardsToDisplay.Count; i++)
         {
             Shard shard = shardsToDisplay[i];
 
-            MenuUIElement element = Instantiate(shardMenuElement).GetComponent<MenuUIElement>();
+            ShardMenuUIElement element = Instantiate(shardMenuElement).GetComponent<ShardMenuUIElement>();
             element.transform.parent = group.transform;
-            element.transform.position = initialPosition.position + i % cols * Vector3.right * xspacing + ((i / (rows))) * Vector3.down * yspacing;
+            Debug.Log((i % cols) + " " + (i / rows));
+            element.transform.position = initialPosition.position + ((i % cols) * Vector3.right * xspacing) + ((i / rows) * Vector3.down * yspacing);
             element.parentMenu = this;
             element.parentGroup = group;
+            element.pauseMenuUI = pauseMenuUI;
+            element.parentMenuOnSelectMode = ParentMenuStatusPostSelect.none;
+            element.shardData = shard;
             group.menuElements.Add(element);
-
         }
         base.Init();
+    }
+
+    public override void Open()
+    {
+        if (group.menuElements.Count == 0)
+        {
+            pauseMenuUI.ShowDescription("I haven't found any shards yet...", CharacterExpression.frown);
+        }
+        else
+        {
+            Debug.Log("focusing shard");
+            group.FocusElement(group.currentMenuElementIndex);
+        }
+        base.Open();
     }
 }
