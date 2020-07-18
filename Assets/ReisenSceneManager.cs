b4ddea7157e93631e9ee966629547aa529094f41
FourSwordKirby <rogliu26@gmail.com>
2020-07-18 15:03:31 -0700

    scene cleanup

--- Assets/ReisenSceneManager.cs
+++ Assets/ReisenSceneManager.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class ReisenSceneManager : MonoBehaviour
 {
+    public Bounds CameraBounds;
     public List<SceneSwitchTrigger> sceneEntrances;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.gray - Color.black * 0.6f;
+        Gizmos.DrawCube(CameraBounds.center, CameraBounds.extents * 2);  
+    }
 }
