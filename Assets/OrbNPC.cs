6742d73a811cc1b03005ad240588b68a915aaaec
FourSwordKirby <rogliu26@gmail.com>
2020-07-19 17:04:19 -0700

    ending orb set up and misc dialogue changes

---
+++ Assets/OrbNPC.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbNPC : Npc
+{
+    public void Stage_000()
+    {
+        GameProgress.Orb.Stage = 001;
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("ending game via orb");
+    }
+}
