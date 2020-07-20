28d412ef9ea2f7af2e49ce33f59c10ce0da1093b
Swofty <swiftstick@gmail.com>
2020-07-19 22:40:03 -0700

    Lunar Reisen WIP 2

--- Assets/NPCs/QuestManagers/LunarReisenQuestManager.cs
+++ Assets/NPCs/QuestManagers/LunarReisenQuestManager.cs
@@ -12,5 +12,33 @@
         Stage = 002;
     }
 
+    public void LunarReisen_Stage002()
+    {
+        Stage = 003;
+    }
 
+    public void LunarReisen_Stage003_Elixir()
+    {
+        TransformToNormalSprite();
+        Stage = 1200;
+    }
+
+    public void LunarReisen_Stage003_Smartphone()
+    {
+        GameProgress.Player.AddShard(Shard.LunarReisen_Smartphone);
+        GameProgress.Player.Smartphone = Assignment.LunarReisen;
+        GameProgress.Player.Magazine = Assignment.Inventory;
+        Stage = 100;
+    }
+
+    public void LunarReisen_Stage003_Wrench()
+    {
+        GameProgress.Player.Wrench = Assignment.LunarReisen;
+        // No reward, no stage advancement.
+    }
+
+    public void LunarReisen_Stage100()
+    {
+        Stage = 101;
+    }
 }
