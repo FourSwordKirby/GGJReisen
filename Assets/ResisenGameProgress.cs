1b1368176fc480671cbc1fb31fc02cd620e8ebe0
FourSwordKirby <rogliu26@gmail.com>
2020-07-21 22:55:43 -0700

    more npc refactor, made miyoi's dialogue functional and punctuated

--- Assets/ResisenGameProgress.cs
+++ Assets/ResisenGameProgress.cs
@@ -184,7 +184,7 @@ public class ReisenGameProgress: GameProgress
     public ReisenNpcCharacterProgress Miyoi;
     public ReisenNpcCharacterProgress Kogasa;
     public int KogasaCount;
-    public List<int> MiyoiReadStages = new List<int>();
+    public int MiyoiShardSeenCount;
 
     public const string KeineStage = "keine_stage";
     public const string KosuzuStage = "kosuzu_stage";
