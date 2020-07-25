d70bdb7473289a79190c600eeaf1ff492a669a98
RogPodge <roliu@microsoft.com>
2020-07-25 01:29:23 -0700

    font improvements, UI cleanups

--- Assets/ReisenGameManager.cs
+++ Assets/ReisenGameManager.cs
@@ -150,7 +150,7 @@ public class ReisenGameManager : MonoBehaviour
 
             // Item ownership checks
             if (variable == ReisenGameProgress.HasCough)
-                conditionalResult = gameProgress.Player.CoughingMedicine == Assignment.Inventory;
+                conditionalResult = gameProgress.Player.CoughMedicine == Assignment.Inventory;
             else if (variable == ReisenGameProgress.HasTextbook)
                 conditionalResult = gameProgress.Player.TextBook == Assignment.Inventory;
             else if (variable == ReisenGameProgress.HasEncyclopedia)
