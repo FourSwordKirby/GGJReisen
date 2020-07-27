7b7a841aac09d51bf17452c9981e4e51b23f05e6
FourSwordKirby <rogliu26@gmail.com>
2020-07-26 22:09:41 -0700

    peach prefab and audio crediting prefab

---
+++ Assets/SongNotification.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SongNotification : MonoBehaviour
+{
+    [SerializeField]
+    private float displayDuration;
+
+    public Animator songAnimator;
+    public TextMeshProUGUI SongTitle;
+    public TextMeshProUGUI SongArtist;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            StartCoroutine(ShowSongInfo("Placeholder Song Title", "Placeholder Song Artist"));
+        }
+    }
+
+    public IEnumerator ShowSongInfo(string songTitle, string songArtist, float customDisplayDuration = 0.0f)
+    {
+        songAnimator.SetTrigger("Show");
+        SongTitle.text = songTitle;
+        SongArtist.text = songArtist;
+
+        if (customDisplayDuration == 0)
+            customDisplayDuration = displayDuration;
+        yield return new WaitForSeconds(customDisplayDuration);
+        songAnimator.SetTrigger("Hide");
+    }
+}
