69fa99e060c90f5648820d2f28e25e5338c14b68
RogPodge <roliu@microsoft.com>
2020-08-23 00:34:36 -0700

    updated the rest of the characters, title screen animations

--- Assets/UI/UIScriptHell/StartMenuUI.cs
+++ Assets/UI/UIScriptHell/StartMenuUI.cs
@@ -115,6 +115,7 @@ public class StartMenuUI : MenuUI
     {
         group.menuElements.Add(element);
         element.transform.position = menuElementPositions[index].position;
+        element.transform.parent = menuElementPositions[index];
     }
 
 }
