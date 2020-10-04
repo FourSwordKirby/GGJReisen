38a09ed17b15950b583d082c314191c557e5a1f8
RogPodge <roliu@microsoft.com>
2020-10-04 14:45:16 -0700

    updated wall generator to not break, updated controls

---
+++ Assets/UI/UIScriptHell/MouseClickDisableUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseClickDisableUI : MonoBehaviour
+{
+    GameObject lastselect;
+    void Start()
+    {
+        lastselect = new GameObject();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current == null)
+            return;
+
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastselect);
+        }
+        else
+        {
+            lastselect = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+}
